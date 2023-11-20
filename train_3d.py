bf0aed6ebd2bb8200f5534f2db5d4defd0e67596
Clarivy <zqc.sc.hn@gmail.com>
2023-11-19 21:34:04 -0800

    chore: adjust to coarse-to-fine training

--- train_3d.py
+++ train_3d.py
@@ -2,15 +2,15 @@ import lightning as L
 import torch
 import numpy as np
 from data_loader import getRaysDataLoaders
-from model import NeRFModule
+from model import CoarseToFineNeRFModule
 
 train_data_loader, val_data_loader = getRaysDataLoaders(
-    "data/lego_200x200.npz", batch_size=2048
+    "data/lego_200x200.npz", batch_size=1024
 )
 
 
 trainer = L.Trainer()
 
-nerf_module = NeRFModule(lr=5e-4)
+nerf_module = CoarseToFineNeRFModule(lr=5e-4)
 
 trainer.fit(nerf_module, train_data_loader, val_data_loader)
